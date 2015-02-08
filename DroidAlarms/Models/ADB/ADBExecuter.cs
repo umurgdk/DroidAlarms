19d5376df5584f3945b8e68565809a22ed25d042
Umur Gedik <umurgdk@gmail.com>
2015-02-09 00:11:39 +0200

    ADBExecuter supports "devices", "getprop", "dumpsys alarm"

--- DroidAlarms/Models/ADB/ADBExecuter.cs
+++ DroidAlarms/Models/ADB/ADBExecuter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 
 namespace DroidAlarms.Models.ADB
@@ -30,14 +31,33 @@ namespace DroidAlarms.Models.ADB
 				process.StartInfo.UseShellExecute = true;
 				process.StartInfo.FileName = "adb";
 				process.StartInfo.CreateNoWindow = true;
+				process.StartInfo.RedirectStandardOutput = true;
+				process.StartInfo.Arguments = string.Join(" ", parameters);
+				process.Start();
+
+				string output = process.StandardOutput.ReadToEnd();
+
+				process.WaitForExit();
+
+				return output;
 			} catch (Exception ex) {
-				
+				return "Error: " + ex.Message;
 			}
 		}
 
 		public string Devices ()
 		{
+			return Run ("devices");
+		}
 
+		public string DeviceProp (string deviceId, string propName)
+		{
+			return Run ("shell", "-s", deviceId, "getprop", propName);
+		}
+
+		public string Alarms (string deviceId)
+		{
+			return Run ("shell", "-s", deviceId, "dumpsys", "alarm");
 		}
 	}
 }
