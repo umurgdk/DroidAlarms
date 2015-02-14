e1d7244c9cd4e154456b08396d2c1b2f23e5f025
Umur Gedik <umurgdk@gmail.com>
2015-02-15 00:18:46 +0200

    AlarmsPanel implemented

--- DroidAlarms/Interface/AlarmsPanel.cs
+++ DroidAlarms/Interface/AlarmsPanel.cs
@@ -1,13 +1,46 @@
 ﻿using System;
+using System.Collections.ObjectModel;
+using Eto;
 using Eto.Forms;
+using System.Collections.Generic;
+using DroidAlarms.Models;
 
 namespace DroidAlarms.Interface
 {
 	public class AlarmsPanel : Panel
 	{
+		ObservableCollection<Alarm> Alarms { get; set; }
+		GridView GridView { get; set; }
+
 		public AlarmsPanel ()
 		{
+			Alarms = new ObservableCollection<Alarm> ();
 
+			GridView = new GridView {
+				DataStore = Alarms
+			};
+
+			GridView.Columns.Add (new GridColumn {
+				DataCell = new TextBoxCell { Binding = Binding.Property<Alarm, string>(a => a.Id)},
+				HeaderText = "ID"
+			});
+
+			GridView.Columns.Add (new GridColumn {
+				DataCell = new TextBoxCell { Binding = Binding.Property<Alarm, string> (a => a.TimeText) },
+				HeaderText = "Date"
+			});
+
+			Content = GridView;
+		}
+		
+
+		public void SetAlarms (IEnumerable<Alarm> newAlarms)
+		{
+			Alarms.Clear ();
+
+			foreach (var alarm in newAlarms) {
+				Alarms.Add (alarm);
+			}
 		}
 	}
 }
